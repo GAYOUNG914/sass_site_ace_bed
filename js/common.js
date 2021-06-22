2bdf4aae52690a4c20ce5784ed8b8cb8d354b031
GAYOUNG914 <rkdud91455@gmail.com>
2021-06-23 00:03:34 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -26,8 +26,6 @@ $(function () {
   });
 
   // prev,next 버튼으로 작동하는 스크립트 상수
-
-  // 과제>> 함수로 코드 줄이기, click메서드는 한개만, if조건문으로 prev랑 next 구분
   const marginNumber = 300;
 
   $("img.next").click(function () {
@@ -57,32 +55,33 @@ $(function () {
     });
   });
 
-  $(".swiper_wrap").each(function () {
-    let thisWrap = $(this); //현재 .wrap을 가져오기
-    let itemsNum = $(this).find(".swiper_items"); // 현재 .wrap의 items 선택  $(".items",this);
-    let itemsLeng = itemsNum.length; // 현재 .wrap의 자식요소인 items의 개수 가져오기
-    itemsNum
-      .mouseenter(function () {
-        //.items에 마우스를 올렸을때
-        let thisIndex = $(this).index();
-        $(this).addClass("on");
-        if (thisIndex == 0) {
-          //첫번째 .items 마우스를 올렸을때
-          thisWrap.css("margin-left", "0px");
-        } else if (thisIndex == itemsLeng - 1) {
-          //마지막 .items 마우스를 올렸을때
-          thisWrap.css("margin-left", "-100px");
-        } else {
-          thisWrap.css("margin-left", "-50px");
-        }
-      })
-      .mouseleave(function () {
-        //.items에 마우스를 내렸을때
-        itemsNum.removeClass("on");
-        thisWrap.css("margin-left", "0px");
-      });
-  });
+  // $(".swiper_wrap").each(function () {
+  //   let thisWrap = $(this); //현재 .wrap을 가져오기
+  //   let itemsNum = $(this).find(".swiper_items"); // 현재 .wrap의 items 선택  $(".items",this);
+  //   let itemsLeng = itemsNum.length; // 현재 .wrap의 자식요소인 items의 개수 가져오기
+  //   itemsNum
+  //     .mouseenter(function () {
+  //       //.items에 마우스를 올렸을때
+  //       let thisIndex = $(this).index();
+  //       $(this).addClass("on");
+  //       if (thisIndex == 0) {
+  //         //첫번째 .items 마우스를 올렸을때
+  //         thisWrap.css("margin-left", "0px");
+  //       } else if (thisIndex == itemsLeng - 1) {
+  //         //마지막 .items 마우스를 올렸을때
+  //         thisWrap.css("margin-left", "-100px");
+  //       } else {
+  //         thisWrap.css("margin-left", "-50px");
+  //       }
+  //     })
+  //     .mouseleave(function () {
+  //       //.items에 마우스를 내렸을때
+  //       itemsNum.removeClass("on");
+  //       thisWrap.css("margin-left", "0px");
+  //     });
+  // });
 
+  //a태그 비활성화
   $('a[href="#"]').click(function (e) {
     e.preventDefault();
   });
